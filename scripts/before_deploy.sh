26bc72cef8d0a363f4278627b0f291b76a71f833
Harri Hämäläinen <hhamalai@iki.fi>
2018-07-01 03:41:28 +0300

    Copy correct path

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -74,7 +74,7 @@ mk_tarball() {
 
     # TODO update this part to copy the artifacts that make sense for your project
     # NOTE All Cargo build artifacts will be under the 'target/$TARGET/{debug,release}'
-    cp target/$TARGET/release/hello $td
+    cp target/$TARGET/release/linkbin $td
 
     pushd $td
 
